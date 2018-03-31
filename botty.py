6285b9b8e7cc0ea205601a4341328556d0553ec2
Artem Vinogradov <artem.vinogradov@phystech.edu>
2018-03-31 21:45:15 +0300

    wtf

---
+++ botty.py
@@ -0,0 +1,45 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+import requests
+from time import sleep
+url = "https://api.telegram.org/bot543653316:AAG-sA0MzH_VREDapooova8dsHCxm4c6S4I/"
+
+
+def get_updates_json(request):
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+
+def last_update(data):
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+def get_chat_id(update):  
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):  
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+
+# In[4]:
+
+
+def main():  
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+        sleep(1)       
+
+if __name__ == '__main__':  
+    main()
+
